4992c7d9dcf8e7eaa9ed1272a6ba0fe2bfe91a05
lksngy <luk.nagy@gmail.com>
2020-12-19 20:39:25 +0100

    update after watching udemy video

--- 03-dynamic-time/script.js
+++ 03-dynamic-time/script.js
@@ -1,9 +1,7 @@
 
 let button = document.querySelector('button');
 
-
 //on click, check the time and output the massage based on time
-
 
 //console.log('ahoj')
 let time = new Date;
@@ -13,16 +11,25 @@ console.log(hours);
 
 function checkTime (){
     let output = document.querySelector('.output');
+    let message;
     console.log(hours);
-    if (hours > 0 && hours < 12) {
-        return output.innerHTML = 'good morning';
-    } else if (hours >= 12 && hours <=17) {
-        return output.innerHTML = `good afternoon`;
+    if (hours > 17) {
+        output.style.backgroundColor = 'red';
+        message = 'good evening';
+    } else if (hours > 12) {
+        output.style.backgroundColor = 'green';
+        message = `good afternoon`;
+    } else if (hours > 0) {
+        output.style.backgroundColor = 'yellow';
+        message = `good morning`;
     } else {
-        return output.innerHTML = `good evening`;
+        output.style.backgroundColor = 'blue';
+        message = `something is wrong here`;
          
     }
+    output.innerHTML = message; 
     
 }
 
 button.addEventListener('click', checkTime);
+
