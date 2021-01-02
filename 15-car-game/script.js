4b8ee5cd80688a50318cea5820e41936c0e1ecd5
lksngy <luk.nagy@gmail.com>
2021-01-02 22:50:31 +0100

    not finished

--- 15-car-game/script.js
+++ 15-car-game/script.js
@@ -1,1 +1,26 @@
 console.log('script');
+
+const score = document.querySelector('.score');
+const startScreen = document.querySelector('.startScreen');
+const gameArea = document.querySelector('.gameArea');
+
+startScreen.addEventListener('click', start);
+
+document.addEventListener('keydown', pressOn);
+document.addEventListener('keyup', pressOff);
+
+function pressOn (e) {
+    e.preventDefault();
+    console.log('on',e.key);
+}
+
+function pressOff (e) {
+    e.preventDefault();
+    console.log('off', e.key);
+}
+
+function start () {
+    console.log('clicked');
+}
+
+ 
