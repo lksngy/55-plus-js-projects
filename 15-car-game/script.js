e534947c43b3633c3b020383fbaae2c6d55927c2
lksngy <luk.nagy@gmail.com>
2021-01-03 17:57:40 +0100

    update

--- 15-car-game/script.js
+++ 15-car-game/script.js
@@ -3,24 +3,47 @@ console.log('script');
 const score = document.querySelector('.score');
 const startScreen = document.querySelector('.startScreen');
 const gameArea = document.querySelector('.gameArea');
+const test = document.querySelector('.test');
+const player = {};
+
+let keys = {
+    ArrowUp: false,
+    ArrowDown: false,
+    ArrowLeft: false,
+    ArrowRight: false
+}
 
 startScreen.addEventListener('click', start);
-
 document.addEventListener('keydown', pressOn);
 document.addEventListener('keyup', pressOff);
 
+function playGame () {
+    console.log('inplay');
+    if(player.start) {
+    window.requestAnimationFrame(playGame);
+    }
+}
+
 function pressOn (e) {
     e.preventDefault();
-    console.log('on',e.key);
+    keys[e.key] = true;
+    console.log(keys);
 }
 
 function pressOff (e) {
     e.preventDefault();
-    console.log('off', e.key);
+    keys[e.key] = false;
+    console.log(keys);
 }
 
 function start () {
-    console.log('clicked');
+    startScreen.classList.add('hide');
+    gameArea.classList.remove('hide');
+    player.start = true;
+    window.requestAnimationFrame(playGame);
+    let car = document.createElement('div');
+    car.innerText = 'Car';
+    gameArea.appendChild(car);
 }
 
  
