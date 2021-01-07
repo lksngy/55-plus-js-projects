0b3f9e2d37d1f0097ee2f1b25049b5725b84a435
lksngy <luk.nagy@gmail.com>
2021-01-07 19:07:42 +0100

    not finished yet

--- 15-car-game/script.js
+++ 15-car-game/script.js
@@ -18,32 +18,46 @@ document.addEventListener('keydown', pressOn);
 document.addEventListener('keyup', pressOff);
 
 function playGame () {
-    console.log('inplay');
+    let car = document.querySelector('.car');
+    let road = gameArea.getBoundingClientRect();
+    console.log(road);
     if(player.start) {
-    window.requestAnimationFrame(playGame);
+        if (keys.ArrowUp &&) {player.y -= player.speed;}
+        if (keys.ArrowDown && player.y < (road.bottom - 100)) {player.y += player.speed;}
+        if (keys.ArrowLeft && player.x > 0) {player.x -= player.speed;}
+        if (keys.ArrowRight && player.x < (road.width - 50)) {player.x += player.speed;}
+        // console.log(keys.ArrowUp);
+        // console.log(player.speed);
+        car.style.left = player.x + 'px';
+        car.style.top = player.y + 'px';
+        score.innerHTML = `[x:${player.x}, y:${player.y}]`;
+        window.requestAnimationFrame(playGame);
     }
 }
 
 function pressOn (e) {
     e.preventDefault();
     keys[e.key] = true;
-    console.log(keys);
+    //console.log(keys);
 }
 
 function pressOff (e) {
     e.preventDefault();
     keys[e.key] = false;
-    console.log(keys);
+    //console.log(keys);
 }
 
 function start () {
     startScreen.classList.add('hide');
     gameArea.classList.remove('hide');
     player.start = true;
+    player.speed = 5;
     window.requestAnimationFrame(playGame);
     let car = document.createElement('div');
     car.innerText = 'Car';
+    car.setAttribute('class', 'car');
     gameArea.appendChild(car);
+    player.x = car.offsetLeft;
+    player.y = car.offsetTop;
+    console.log(player);
 }
-
- 
