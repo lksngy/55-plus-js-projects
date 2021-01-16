b99711bed00d1df3475ab48a1aff8595abbf49b2
lksngy <luk.nagy@gmail.com>
2021-01-16 23:32:08 +0100

    not working

--- 20-csv-maker/script.js
+++ 20-csv-maker/script.js
@@ -7,6 +7,10 @@ button.addEventListener('click', function(){
 })
 
 let url;
+// fucking not working :/
+const feedID = '2PACX-1vR-iHOQPLgOehU_ZEKOvPlg7x';
+const feed = 'https://spreadsheets.google.com/feeds/cells/' + feedID + '/1/public/full?alt=json';
+console.log(feed);
 let myData = [
     ['Title', 'Content'],
     ['Row1', 'Content1'],
@@ -14,9 +18,19 @@ let myData = [
     ['Row2', 'Content3'],
 
 ];
-
+// some stupid function to clean up the data
 function clean(row){
-
+    let rep = "";
+    row.forEach(function (cell, index){
+        console.log(cell);
+        cell = cell == null ? "" : cell.toString();
+        if (cell.search(/("|,|\n)/g) >= 0) cell = '"' + cell + '"';
+        if (index > 0){
+            rep+= ",";
+        }
+        rep += cell;
+    })
+    return rep;
 }
 
 function createCSV(data) {
