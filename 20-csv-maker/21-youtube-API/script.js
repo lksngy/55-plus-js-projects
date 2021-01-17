7bc1e56281122573e5265aa19fdab9af9e85a1f8
lksngy <luk.nagy@gmail.com>
2021-01-17 13:24:43 +0100

    fetch done, now use input to modify apiUrl and then show results on the page

--- 20-csv-maker/21-youtube-API/script.js
+++ 20-csv-maker/21-youtube-API/script.js
@@ -4,21 +4,30 @@ let input = document.querySelector('input');
 let button = document.querySelector('button');
 let output = document.querySelector('.output');
 
-console.log(input);
 
 const api = 'AIzaSyAOQoVhNbQT3wOfDb2DrfSp-7uebk2airE';
 const apiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=' + api + '&q=test';
-console.log(apiUrl);
+//console.log(apiUrl);
+let responseObj;
 let responseArray = [];
 
-// function displayResponse () {};
+function displayResponse (video) {
+    console.log(responseArray.length);
+    responseArray.forEach((video) => {
+        console.log(video.snippet.title);
+    })
+    
+    
+};
 
 async function ySearch () {
     try {
         const response = await fetch(apiUrl);
-        responseArray = await response.json();
+        responseObj = await response.json();
+        console.log(responseObj);
+        responseArray = responseObj.items;
         console.log(responseArray);
-        // display response function call
+        displayResponse();
     } catch (error) {
         console.log('ups!', error);
     }
@@ -26,8 +35,10 @@ async function ySearch () {
 
 
 button.addEventListener('click', function(){
+    
     output.innerHTML = '';
     let text = input.value;
+    
     let box = document.createElement('div');
     box.innerText = text;
     box.classList.add('box');
