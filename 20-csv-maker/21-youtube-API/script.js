2078293ac1128646ae7ae6ca8680515edca689d1
lksngy <luk.nagy@gmail.com>
2021-01-17 19:28:59 +0100

    done with my own styling, my own modified input, my own way of showing search results

--- 20-csv-maker/21-youtube-API/script.js
+++ 20-csv-maker/21-youtube-API/script.js
@@ -6,7 +6,7 @@ let output = document.querySelector('.output');
 
 
 const api = 'AIzaSyAOQoVhNbQT3wOfDb2DrfSp-7uebk2airE';
-const apiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=' + api + '&q=test';
+let apiUrl = '';
 //console.log(apiUrl);
 let responseObj;
 let responseArray = [];
@@ -14,7 +14,24 @@ let responseArray = [];
 function displayResponse (video) {
     console.log(responseArray.length);
     responseArray.forEach((video) => {
-        console.log(video.snippet.title);
+        //console.log(video.snippet.title);
+        //console.log(video.snippet.description);
+        let box = document.createElement('div');
+        box.classList.add('box');
+        output.appendChild(box);
+        let title = document.createElement('div');
+        let desc = document.createElement('div');
+        title.innerText = `${video.snippet.title} `;
+        title.classList.add('title');
+        desc.innerText = `${video.snippet.description}`;
+        box.appendChild(title);
+        box.appendChild(desc);
+        let img = document.createElement('img');
+        img.setAttribute('src', video.snippet.thumbnails.medium.url);
+        img.setAttribute('title', video.snippet.title);
+        img.setAttribute('alt', video.snippet.description);
+        box.appendChild(img);
+
     })
     
     
@@ -35,14 +52,9 @@ async function ySearch () {
 
 
 button.addEventListener('click', function(){
-    
     output.innerHTML = '';
     let text = input.value;
-    
-    let box = document.createElement('div');
-    box.innerText = text;
-    box.classList.add('box');
-    //console.log(box.innerText);
-    output.appendChild(box);
+    apiUrl = 'https://youtube.googleapis.com/youtube/v3/search?part=snippet&maxResults=20&key=' + api + '&q=' + text;
+    console.log(apiUrl);
     ySearch();
     })
