c17af93d5a05b013697f8c6d9200a0014d7c19de
lksngy <luk.nagy@gmail.com>
2021-01-19 23:50:21 +0100

    sh*t

--- 22-black-jack/script.js
+++ 22-black-jack/script.js
@@ -12,33 +12,55 @@ const deal = document.querySelector('.deal');
 const hit = document.querySelector('.hit');
 const stand = document.querySelector('.stand');
 
+const side = ['dealerCards', 'playerCards'];
 const ranks = [2,3,4,5,6,7,8,9,'J','Q','K','A'];
 const suits = ['hearts', 'diams', 'clubs', 'spades'];
-let deck = [];
+let deck = [{
+    suit: 'hearts',
+    rank: 2,
+    value: 2
+}];
 
+let playing = false;
 
 function buildDeck() {
-    deck = [];
-    for (i = 0; i< suits.length; i++) {
-        for (j=0; j< ranks.length; j++) {
-            let card = {};
-            card.suit = suits[i];
-            card.rank = ranks[j];
-            // card.value
-            deck.push(card);
-        }
-    }
-    console.log(deck);
+    // for (i = 0; i< suits.length; i++) {
+    //     for (j=0; j< ranks.length; j++) {
+    //         let card = {};
+    //         card.suit = suits[i];
+    //         card.rank = ranks[j];
+    //         // card.value
+    //         deck.push(card);
+    //     }
+    // }
+    // //console.log(deck);
 }
 
 function toggleButtons () {
     deal.classList.toggle('hideButton');
-    hit.classList.toggle('hideButton');
-    stand.classList.toggle('hideButton');
+    //hit.classList.toggle('hideButton');
+    //stand.classList.toggle('hideButton');
 }
 
-function makeCard () {
-    // create visual, show
+
+function makeCard (who) {
+    let visual1 = deck[0].rank + '<br />&' + deck[0].suit + ';';
+    console.log(visual1);
+    let visual2 = deck[0].rank + '&' + deck[0].suit + ';';
+    console.log(visual2);
+    let div = document.createElement('div');
+    div.setAttribute('class', 'card');
+    dealerCards.appendChild(div);
+
+    let span1 = document.createElement('span');
+    span1.setAttribute('class', 'tiny');
+    span1.innerHTML = visual2;
+    div.appendChild(span1);
+
+    let span2 = document.createElement('span');
+    span2.setAttribute('class', 'big');
+    span2.innerHTML = visual1;
+    div.appendChild(span2);
 }
 
 function drawCard () {
@@ -47,12 +69,24 @@ function drawCard () {
 
 function playGame () {
     // start
+    if (playing === false) {
+        playing = true;
+        makeCard();
+        makeCard();
+        toggleButtons();
+    }
+    
+
+    
+    
     // play
     // finish
 }
 
+deal.addEventListener('click', playGame);
 
-buildDeck();
+//buildDeck();
+
 // 3 stages of game - start, play, finish(start over)
 
 // start => clean up everything, call a new game => play
